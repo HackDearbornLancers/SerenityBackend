10f2005d282ff00791f3c04ffcfe2d6ea8f29469
JustinTheGreat <marcusgong123@gmail.com>
2023-10-21 17:06:46 -0400

    requirements

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from sentiment import simple_page
 
 #how to make a requirements.txt
 
 app = Flask(__name__)
+
+app.register_blueprint(simple_page)
 
 @app.route("/")
 def hello_world():
@@ -10,3 +13,4 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(port=5000)
+
--- sentiment.py
+++ sentiment.py
@@ -6,9 +6,6 @@ from flask import Blueprint, render_template, abort
 simple_page = Blueprint('simple_page', __name__,template_folder='templates')
 
 
-@app.route("/")
+@simple_page.route("/s")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
-    app.run(port=5000)
