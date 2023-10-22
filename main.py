e8152a800766c7bcd36de5f5f03d1ba89b2c1683
Genie-7 <81450268+Genie-7@users.noreply.github.com>
2023-10-21 23:51:24 -0400

    response api

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from response import api_blueprint, generate_openai_response
 from sentiment import simple_page
+<<<<<<< Updated upstream
 
 load_dotenv()
+=======
+from response import create_initial_conversation, generate_openai_response
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/v1')  # Register the blueprint with a URL prefix
@@ -12,12 +16,21 @@ app.register_blueprint(simple_page)
 @app.route('/openai', methods=['POST'])
 def openai_endpoint():
     request_data = request.get_json()
+<<<<<<< Updated upstream
     response = generate_openai_response(request_data['message'])
     return jsonify({"response": response})
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+=======
+    print(request_data)
+    user_message = request_data['message']
+    print(user_message)
+    conversation = create_initial_conversation()
+    response = generate_openai_response(user_message, conversation)
+    return jsonify({"response": response})
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     app.run(port=5000)
--- response.py
+++ response.py
@@ -1,24 +1,36 @@
-from flask import Blueprint, request, jsonify
-from flask import Flask, request, jsonify
 import os
 import openai
+from dotenv import load_dotenv
 
-api_blueprint = Blueprint("api", __name__)
+load_dotenv()
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
-openai.api_key = OPENAI_API_KEY
+if api_key is None:
+    raise EnvironmentError("API key not found in .env file")
 
-def generate_openai_response(message):
+openai.api_key = api_key
+
+def create_initial_conversation():
+    # Create and return an initial conversation with a system message
+    return [{"role": "system", "content": "You are an empathetic therapist."}] 
+
+def generate_openai_response(message, conversation):
+    conversation.append({"role": "user", "content": message})
     try:
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=message,
-            max_tokens=50,
-            n=1,
-            stop=None
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+            temperature=0.6,  # Controls randomness in responses
+            max_tokens=20,    # Limits the response length
+            stop=None         # Ensure the response doesn't end prematurely
         )
+        completion = response.choices[0].message["content"]
+        conversation.append({"role": "assistant", "content": completion})  # Store assistant's response
+        return completion
 
-        return response.choices[0].text.strip()
+
     except Exception as e:
         return str(e)
+
+    
